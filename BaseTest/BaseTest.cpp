166dee4f26b93a7e26c370c1391ff5d4543e30a2
SwartzMss <swartz_lubel@outlook.com>
2017-05-27 23:09:11 +0800

    增加了dbgview的调试输出
    Signed-off-by: SwartzMss <swartz_lubel@outlook.com>

--- BaseTest/BaseTest.cpp
+++ BaseTest/BaseTest.cpp
@@ -3,14 +3,27 @@
 
 #include "stdafx.h"
 #include "BaseLib.h"
-
+#include <windows.h>
 #pragma comment(lib, "Bin/BaseLib.lib")
+
+void OutputDebugPrintf(const char * strOutputString, ...)
+{
+	char strBuffer[4096] = { 0 };
+	va_list vlArgs;
+	va_start(vlArgs, strOutputString);
+	_vsnprintf(strBuffer, sizeof(strBuffer) - 1, strOutputString, vlArgs);
+	//vsprintf(strBuffer,strOutputString,vlArgs);
+	va_end(vlArgs);
+	OutputDebugString(strBuffer);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	IBaseLib* pBaseLib = CreateBaseLib();
 	int nret = pBaseLib->Add(10, 20);
+	OutputDebugPrintf("DEBUGINFO| result = %d", nret);
 	nret = pBaseLib->Sub(100, 20);
+	OutputDebugPrintf("DEBUGINFO| result = %d", nret);
 	DestroyBaseLib(pBaseLib);
 	return 0;
 }
